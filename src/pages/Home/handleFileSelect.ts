b54b527f51e5365814227eedecb8d52cf81a0c23
Luke <lucian.io@outlook.com>
2023-10-23 15:48:03 -0400

    tauri dialog installed

--- src/pages/Home/handleFileSelect.ts
+++ src/pages/Home/handleFileSelect.ts
@@ -1,17 +1,24 @@
-export const handleFileSelect = (index: number) => {
-  const fileInput = document.createElement("input")
-  fileInput.type = "file"
-  fileInput.accept = ".mp3"
-  fileInput.style.display = "none"
-  fileInput.addEventListener("change", (event) => {
-    const selectedFile = (event.target as HTMLInputElement).files?.[0]
-    if (selectedFile) {
+import {open} from "@tauri-apps/api/dialog"
+
+export const handleFileSelect = async (index: number) => {
+  try {
+    const result = await open({
+      filters: [
+        {
+          name: "MP3 Files",
+          extensions: ["mp3"],
+        },
+      ],
+    })
+    if (result && result.length > 0) {
+      const selectedFile = result[0]
       if (index === 1) {
         localStorage.setItem("selectedMP31", JSON.stringify(selectedFile))
       } else if (index === 2) {
         localStorage.setItem("selectedMP32", JSON.stringify(selectedFile))
       }
     }
-  })
-  fileInput.click()
+   } catch (error) {
+    console.error(error)
+  }
 }
