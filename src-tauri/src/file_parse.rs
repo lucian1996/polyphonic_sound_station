3e8409fb021171be32b378280eb3e9a3163f1e48
Luke <lucian.io@outlook.com>
2023-10-23 22:48:34 -0400

    rust reads metadata

--- src-tauri/src/file_parse.rs
+++ src-tauri/src/file_parse.rs
@@ -1,16 +1,27 @@
-use serde::Serialize;
-use std::fs;
-use tauri::Error as InvokeError;
+use audiotags::Tag;
 
-#[derive(Serialize)]
-pub struct FileMetadata {
-    size: u64,
+fn read_audio_metadata(file_path: &str) -> Option<Metadata> {
+    // Create a Tag instance and read metadata from the audio file.
+    let tag = Tag::new().read_from_path(file_path).ok()?;
+
+    // Extract the desired metadata fields.
+    let title = tag.title().map(String::from);
+    let artist = tag.artist().map(String::from);
+    // You can also retrieve album cover image if it exists.
+
+    Some(Metadata { title, artist })
+}
+
+struct Metadata {
+    title: Option<String>,
+    artist: Option<String>,
 }
 
 #[tauri::command]
-pub fn parse_metadata(path: String) -> Result<FileMetadata, InvokeError> {
-    let metadata = fs::metadata(path)?;
-    Ok(FileMetadata {
-        size: metadata.len(),
-    })
+pub fn parse_metadata(path: &str) {
+    let file_path = path;
+    if let Some(metadata) = read_audio_metadata(file_path) {
+        println!("Title: {:?}", metadata.title);
+        println!("Artist: {:?}", metadata.artist);
+    }
 }
