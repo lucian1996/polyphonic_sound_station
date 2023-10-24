651c547fb89a084771cfbcb5361cc9b6a6807c8d
Luke <lucian.io@outlook.com>
2023-10-24 00:27:07 -0400

    path updated

--- src-tauri/src/file_parse.rs
+++ src-tauri/src/file_parse.rs
@@ -1,27 +1,39 @@
 use audiotags::Tag;
-
-fn read_audio_metadata(file_path: &str) -> Option<Metadata> {
-    // Create a Tag instance and read metadata from the audio file.
-    let tag = Tag::new().read_from_path(file_path).ok()?;
-
-    // Extract the desired metadata fields.
-    let title = tag.title().map(String::from);
-    let artist = tag.artist().map(String::from);
-    // You can also retrieve album cover image if it exists.
-
-    Some(Metadata { title, artist })
-}
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
 struct Metadata {
     title: Option<String>,
     artist: Option<String>,
+    file_path: String,
+}
+
+lazy_static! {
+    static ref METADATA: Mutex<Option<Metadata>> = Mutex::new(None);
+}
+
+fn read_audio_metadata(file_path: String) {
+    let tag = Tag::new().read_from_path(&file_path).ok();
+    let title = tag.as_ref().and_then(|t| t.title()).map(String::from);
+    let artist = tag.as_ref().and_then(|t| t.artist()).map(String::from);
+
+    // Update the global METADATA
+    let mut metadata = METADATA.lock().unwrap();
+    *metadata = Some(Metadata {
+        title,
+        artist,
+        file_path,
+    });
 }
 
 #[tauri::command]
 pub fn parse_metadata(path: &str) {
-    let file_path = path;
-    if let Some(metadata) = read_audio_metadata(file_path) {
-        println!("Title: {:?}", metadata.title);
-        println!("Artist: {:?}", metadata.artist);
-    }
+    let file_path = path.to_string(); // Convert &str to String
+    read_audio_metadata(file_path.clone()); // Clone the path to avoid moving it
+
+    // Read the global METADATA
+    let metadata = METADATA.lock().unwrap();
+    println!("Title: {:?}", metadata.as_ref().unwrap().title);
+    println!("Artist: {:?}", metadata.as_ref().unwrap().artist);
+    println!("Path: {:?}", metadata.as_ref().unwrap().file_path);
 }
